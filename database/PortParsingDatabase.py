bcb0abe56a157fcd82f5203da076e52365edfad4
Muhammadamin <mansurov12133@gmail.com>
2026-08-28 07:38:50 +0800

    Insert open port to db

--- database/PortParsingDatabase.py
+++ database/PortParsingDatabase.py
@@ -4,4 +4,19 @@ class ParsePortsDb:
     def __init__(self, db_file = 'ports.db'):
         self.db_file = db_file
         self.conn = sqlite3.connect(self.db_file)
+        self.cursor = self.conn.cursor()
+
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS open_ports ( 
+                id INTIGEER PRIMARY KEY AUTOINCRIMENT,
+                ip_address TEXT NOT NULL,
+                port INTEGER NOT NULL,
+                scan_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        ''')
+        self.conn.commit()
+
+    def InsertOpenPort(self, ip_address, port):
+        self.cursor.execute('INSERT INTO open_ports (ip_address, port, scan_timestamp) VALUES (?, ?, ?)',
+    (ip_address, port)
+    )
+        self.conn.commit()
 
