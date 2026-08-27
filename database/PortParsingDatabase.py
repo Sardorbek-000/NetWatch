ae9a55241be80cbbc5d84f51f802da3d5d4d15be
Muhammadamin <mansurov12133@gmail.com>
2026-08-28 07:57:42 +0800

    Databse connect with port parser and can save to database 'ports.db'

--- database/PortParsingDatabase.py
+++ database/PortParsingDatabase.py
@@ -1,22 +1,25 @@
 import sqlite3
+import threading
 
 class ParsePortsDb:
     def __init__(self, db_file = 'ports.db'):
         self.db_file = db_file
-        self.conn = sqlite3.connect(self.db_file)
+        self.conn = sqlite3.connect(self.db_file, check_same_thread=False)
         self.cursor = self.conn.cursor()
+        self.lock = threading.Lock()
 
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS open_ports ( 
-                id INTIGEER PRIMARY KEY AUTOINCRIMENT,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 ip_address TEXT NOT NULL,
                 port INTEGER NOT NULL,
-                scan_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                scan_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)
         ''')
         self.conn.commit()
 
     def InsertOpenPort(self, ip_address, port):
-        self.cursor.execute('INSERT INTO open_ports (ip_address, port, scan_timestamp) VALUES (?, ?, ?)',
-    (ip_address, port)
-    )
-        self.conn.commit()
+        with self.lock:
+            self.cursor.execute('INSERT INTO open_ports (ip_address, port) VALUES (?, ?)',
+        (ip_address, port)
+        )
+            self.conn.commit()
 
